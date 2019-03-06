0c0e45577ef4f23e3467554e92a6fd13da50c16e
Tri Suliswanto <3sulis12@gmail.com>
2019-03-07 01:15:34 +0700

    animation led

--- I2C_Master_PWM.ino
+++ I2C_Master_PWM.ino
@@ -1,5 +1,13 @@
 #include <Wire.h>
 
+#define JUMLAH_SLAVE_DEVICE 6
+#define JUMLAH_PIN_SETIAP_SLAVE 9
+#define TOTAL_JUMLAH_PIN  (JUMLAH_SLAVE_DEVICE * JUMLAH_PIN_SETIAP_SLAVE)
+#define JUMLAH_MAX_RGB  (TOTAL_JUMLAH_PIN/3)
+#define JUMLAH_MAX_RGBW  (TOTAL_JUMLAH_PIN/4)
+
+#define SLAVE_START_ADDRESS 1   // alamat Slave pertama
+
 void setup() {
   Serial.begin(9600);
   Wire.begin(); // join i2c bus (address optional for master)
@@ -7,24 +15,85 @@ void setup() {
 
 void loop() {
 
-  setPWM(1, 9, 50);  
-  setPWM(1, 10, 100);  
-  setPWM(1, 11, 200); 
-  setPWM(2, 13, 50); 
-  Serial.println("oke");
-  delay(1000);
-  setPWM(1, 9, 0);  
-  setPWM(1, 10, 0);  
-  setPWM(1, 11, 0);
-  setPWM(2, 13, 0);
-  Serial.println("oke juga");
-  delay(1000);
-  setPWM(1, 9, 255);  
-  setPWM(1, 10, 255);  
-  setPWM(1, 11, 255);
-  setPWM(2, 13, 255);
-  Serial.println("oke juga banget");
-  delay(1000);
+  //untuk kontrol per pin menggunakan fungsi setPWM
+//nganan
+  for(int i=1; i<=TOTAL_JUMLAH_PIN; i++){
+    if(i-1 > 0) setPWM(i-1, 0);
+    setPWM(i, 250);
+    delay(50);
+  }
+  delay(500);
+//ngiri
+  for(int i=TOTAL_JUMLAH_PIN; i>=1; i--){
+    setPWM(i, 250);
+    if(i+1 <= TOTAL_JUMLAH_PIN) setPWM(i+1, 0);
+    delay(50);
+  }
+  delay(500);
+
+//Untuk animasi menggunakan fungsi setRGB
+//nganan
+  for(int i=1; i<=JUMLAH_MAX_RGB; i++){
+    if(i-1 > 0) setRGB(i-1, 0,0,0);
+    setRGB(i, 250, 250, 250);
+    delay(50);
+  }
+  delay(500);
+//ngiri
+  for(int i=JUMLAH_MAX_RGB; i>=1; i--){
+    setRGB(i, 250, 250, 250);
+    if(i+1 <= JUMLAH_MAX_RGB) setRGB(i+1, 0,0,0);
+    delay(50);
+  }
+  delay(500);
+
+  //Untuk animasi menggunakan fungsi setRGBW
+//nganan
+  for(int i=1; i<=JUMLAH_MAX_RGBW; i++){
+    if(i-1 > 0) setRGBW(i-1, 0,0,0,0);
+    setRGBW(i, 250, 250, 250, 250);
+    delay(50);
+  }
+  delay(500);
+//ngiri
+  for(int i=JUMLAH_MAX_RGBW; i>=1; i--){
+    setRGBW(i, 250, 250, 250, 250);
+    if(i+1 <= JUMLAH_MAX_RGBW) setRGBW(i+1, 0,0,0,0);
+    delay(50);
+  }
+  delay(500);
+
+  //nganan
+  for(int i=1; i<=JUMLAH_MAX_RGBW; i++){
+    setRGBW(i, 250, 250, 250, 250);
+    delay(50);
+  }
+  delay(500);
+//ngiri
+  for(int i=JUMLAH_MAX_RGBW; i>=1; i--){
+    setRGBW(i, 250, 250, 250, 250);
+    if(i+1 <= JUMLAH_MAX_RGBW) setRGBW(i+1, 0,0,0,0);
+    delay(50);
+  }
+  delay(500);
+
+
+// animasi untuk satu block kelompok led
+  uint8_t jumlah_per_block = 10;
+//nganan
+  for(int i=1; i<=TOTAL_JUMLAH_PIN; i+=jumlah_per_block){
+    setBLOCK(i, jumlah_per_block, 250);
+    if(i-jumlah_per_block >= 0) setBLOCK(i-jumlah_per_block, jumlah_per_block, 0);
+    delay(50);
+  }
+  delay(100);
+//ngiri
+  for(int i=TOTAL_JUMLAH_PIN; i>=1; i-=jumlah_per_block){
+    setBLOCK(i, jumlah_per_block, 250);
+    if(i+jumlah_per_block <= TOTAL_JUMLAH_PIN) setBLOCK(i+jumlah_per_block, jumlah_per_block, 0);
+    delay(50);
+  }
+  delay(100);  
 }
 
 
--- i2c_master.ino
+++ i2c_master.ino
@@ -1,8 +1,31 @@
-
-
-void setPWM(uint8_t address, uint8_t pin_number, uint8_t value){
+void setPWM(uint8_t pin_number, uint8_t value){
+  uint8_t address = ceil((float)((float)pin_number/(float)TOTAL_JUMLAH_PIN)*(float)JUMLAH_SLAVE_DEVICE);
+  uint8_t actual_pin_number =  (JUMLAH_PIN_SETIAP_SLAVE - ((address * JUMLAH_PIN_SETIAP_SLAVE)-pin_number))+1;
+  
   Wire.beginTransmission(address);  // transmit to device address
-  Wire.write(pin_number);           // sends one byte
+  Wire.write(actual_pin_number);           // sends one byte
   Wire.write(value);                // sends one byte
   Wire.endTransmission();           // stop transmitting  
+}
+
+//dikelompokkan 1 lampu 3 warna
+void setRGB(uint8_t rgb_number, uint8_t r, uint8_t g, uint8_t b){
+  setPWM((rgb_number*3)-2, r);  // RED
+  setPWM((rgb_number*3)-1, g);  // GREEN
+  setPWM(rgb_number*3, b);     // BLUE
+}
+
+//dikelompokkan 1 lampu 4 warna ==> RGBW
+void setRGBW(uint8_t rgbw_number, uint8_t r, uint8_t g, uint8_t b, uint8_t w){
+  setPWM((rgbw_number*4)-3, r);  // RED
+  setPWM((rgbw_number*4)-2, g);  // GREEN
+  setPWM((rgbw_number*4)-1, b);  // BLUE
+  setPWM(rgbw_number*4, w);     // WHITE / pompa
+}
+
+//dikelompokkan 1 block
+void setBLOCK(uint8_t start_pin_number, uint8_t jml_per_block, uint8_t val){
+  for(int i=0; i<jml_per_block; i++){
+    setPWM(i+start_pin_number, val);  // loop sampai jumlah per block
+  }
 }
